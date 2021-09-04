7fa7193521c335e127711ac201b01e37a4df2a22
dong-geon-Lee <ch9716@naver.com>
2021-09-04 13:25:41 +0900

    back

---
+++ 4344/wellformed.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+let input = fs.readFileSync("./input.txt").toString();
+input = input.split("\n");
+
+console.log(input, "1");
+
+const testCaseNum = Number(input[0]);
+const inputTestCase = [];
+
+for (let i = 1; i <= testCaseNum; ++i) {
+  const arr = input[i].split(" ").map((item) => Number(item));
+  const newArr = [];
+
+  for (let i = 1; i <= arr[0]; ++i) {
+    newArr.push(arr[i]);
+  }
+
+  const testCase = {
+    N: arr[0],
+    arr: newArr,
+  };
+
+  inputTestCase.push(testCase);
+}
+
+console.log("inputTestCase: ", inputTestCase);
+
+console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+
+function solution(C, testCase) {
+  console.log("C : ", C);
+  console.log("testCase: ", testCase);
+}
+
+solution(testCaseNum, inputTestCase);
