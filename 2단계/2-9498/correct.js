2a249ff3c853ecd1364f68560a2f2fd42bea8cfe
dong-geon-Lee <ch9716@naver.com>
2021-09-05 19:24:23 +0900

    2-stage

---
+++ 2단계/2-9498/correct.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+input = input[0];
+input = input.split(" ").map((arr) => +arr);
+
+function solution(score) {
+  if (score >= 90 && score <= 100) {
+    console.log("A");
+  } else if (score >= 80 && score <= 89) {
+    console.log("B");
+  } else if (score >= 70 && score <= 79) {
+    console.log("C");
+  } else if (score >= 60 && score <= 69) {
+    console.log("D");
+  } else {
+    console.log("F");
+  }
+}
+
+solution(input[0]);
